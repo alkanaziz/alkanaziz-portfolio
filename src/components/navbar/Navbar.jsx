3ca841a4a7d4168a6d49a3e45236115e12083945
alkanaziz <alkanaziz@gmail.com>
2024-08-20 13:29:17 +0200

    feat: Add lazy loading for span text in Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,21 @@
 import { FaGithub } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa6';
 import { FaMedium } from 'react-icons/fa6';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
   return (
     <div className="navbar h-24">
       {/* Sidebar */}
       <div className="wrapper m-auto flex h-full max-w-screen-xl items-center justify-between">
-        <span className="font-bold">Web Developer Alkan</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="font-bold"
+        >
+          Web Developer Alkan
+        </motion.span>
         <div className="social flex gap-5">
           <a href="#">
             <FaGithub size={20} />
