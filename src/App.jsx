536260b23ed9e897bab2e5fe131456274ec81125
alkanaziz <alkanaziz@gmail.com>
2024-08-20 10:30:57 +0200

    chore: Update App component to include sections for hero, parallax, services, and portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import './app.scss';
+
 export default function App() {
-  return <h1 className="m-5 text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <section className="h-screen snap-center">Hero</section>
+      <section className="h-screen snap-center">Parallax</section>
+      <section className="h-screen snap-center">Services</section>
+      <section className="h-screen snap-center">Paralax</section>
+      <section className="h-screen snap-center">Portfolio1</section>
+      <section className="h-screen snap-center">Portfolio2</section>
+      <section className="h-screen snap-center">Portfolio3</section>
+      <section className="h-screen snap-center">Contact</section>
+    </div>
+  );
 }
