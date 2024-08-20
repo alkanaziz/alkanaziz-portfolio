0d9e553072874297bac778d493a3654a35f1b281
alkanaziz <alkanaziz@gmail.com>
2024-08-20 19:55:46 +0200

    refactor: Update section IDs in App.jsx for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import Navbar from './components/navbar/Navbar';
 export default function App() {
   return (
     <div>
-      <section className="h-screen snap-center">
+      <section id="homepage" className="h-screen snap-center">
         <Navbar />
       </section>
       <section className="h-screen snap-center">Parallax</section>
-      <section className="h-screen snap-center">Services</section>
+      <section id="services" className="h-screen snap-center">
+        Services
+      </section>
       <section className="h-screen snap-center">Paralax</section>
-      <section className="h-screen snap-center">Portfolio1</section>
+      <section id='portfolio' className="h-screen snap-center">Portfolio1</section>
       <section className="h-screen snap-center">Portfolio2</section>
       <section className="h-screen snap-center">Portfolio3</section>
-      <section className="h-screen snap-center">Contact</section>
+      <section id='about' className="h-screen snap-center">About</section>
+      <section id='contact' className="h-screen snap-center">Contact</section>
     </div>
   );
 }
