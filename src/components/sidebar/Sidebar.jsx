45c32d0cc74bd2d591467a27d762d15249062428
alkanaziz <alkanaziz@gmail.com>
2024-08-20 19:55:28 +0200

    feat: Add Sidebar component with animation to toggle open and closed state

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import Links from './links/Links';
+import ToggleButton from './toggleButton/ToggleButton';
+import { delay, motion } from 'framer-motion';
+
+const variants = {
+  open: {
+    clipPath: 'circle(1200px at calc(100% - 50px) 50px)',
+    transition: {
+      type: 'spring',
+      stiffness: 20,
+    },
+  },
+  closed: {
+    clipPath: 'circle(0px at calc(100% - 50px) 50px)',
+    transition: {
+      type: 'spring',
+      stiffness: 400,
+      damping: 40,
+      delay: 0.5,
+    },
+  },
+};
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <motion.div
+      className="sidebar flex flex-col items-center justify-center bg-slate-300 text-slate-900"
+      animate={open ? 'open' : 'closed'}
+    >
+      <motion.div
+        className="bg fixed bottom-0 right-0 top-0 w-96 bg-slate-300"
+        variants={variants}
+      >
+        <Links setOpen={setOpen} />
+      </motion.div>
+      <ToggleButton setOpen={setOpen} />
+    </motion.div>
+  );
+};
+export default Sidebar;
