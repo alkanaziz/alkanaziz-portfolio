88f4267e3e3accc820c326dd9f2da5e7d1e8f961
alkanaziz <alkanaziz@gmail.com>
2024-08-21 12:04:09 +0200

    refactor: Update Sidebar component styles for better user experience

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -31,7 +31,7 @@ const Sidebar = () => {
       animate={open ? 'open' : 'closed'}
     >
       <motion.div
-        className="bg fixed bottom-0 right-0 top-0 z-10 w-96 bg-gradient-to-b from-slate-300 to-slate-100/30"
+        className="bg fixed bottom-0 right-0 top-0 z-20 w-96 bg-gradient-to-b from-slate-300 to-slate-100/30"
         variants={variants}
       >
         <Links setOpen={setOpen} />
