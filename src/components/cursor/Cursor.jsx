9eea35a2ec6b8ce7f63a9362758384bd63f6f815
alkanaziz <alkanaziz@gmail.com>
2024-08-23 22:16:39 +0200

    refactor: Add Cursor component for mouse tracking

---
+++ src/components/cursor/Cursor.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+
+const Cursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener('mousemove', mouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', mouseMove);
+    };
+  }, []);
+
+  console.log(position);
+
+  return (
+    <motion.div
+      animate={{ x: position.x+10, y: position.y+10 }}
+      className="cursor z-50 fixed size-12 hidden md:block rounded-full border"
+    >
+    </motion.div>
+  );
+};
+export default Cursor;
