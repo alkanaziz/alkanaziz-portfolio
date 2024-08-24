bf503b5656721f829c204f66c1004ff9713375d8
alkanaziz <alkanaziz@gmail.com>
2024-08-24 02:45:11 +0200

    refactor: Improve Parallax component layout for better responsiveness

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -6,7 +6,7 @@ const Parallax = ({ type }) => {
 
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["start start", "end start"]
+    offset: ['start start', 'end start'],
   });
   const yText = useTransform(scrollYProgress, [0, 1], ['0%', '500%']);
   const yBg = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
@@ -14,14 +14,23 @@ const Parallax = ({ type }) => {
   return (
     <div
       ref={ref}
-      className={`parallax overflow-hidden relative flex size-full items-center justify-center bg-gradient-to-b from-slate-800 ${type === 'services' ? 'to-slate-900' : 'to-slate-700'}`}
+      className={`parallax relative flex size-full items-center justify-center overflow-hidden bg-gradient-to-b from-slate-800 ${type === 'services' ? 'to-slate-900' : 'to-slate-700'}`}
     >
-      <motion.h1 style={{ y: yText }} className="text-8xl">
+      <motion.h1
+        style={{ y: yText }}
+        className="text-center text-6xl md:text-8xl"
+      >
         {type === 'services' ? 'What We Do?' : 'What We Did?'}
       </motion.h1>
-      <motion.div className="mountains absolute z-10 size-full bg-[url('/mountains.png')] bg-cover bg-bottom"></motion.div>
-      <motion.div style={{ y: yBg }} className={`planets z-[5] absolute size-full ${type === "services" ? "bg-[url('/planets.png')]" : "bg-[url('/sun.png')]"} bg-cover bg-bottom`}></motion.div>
-      <motion.div style={{ x: yBg }}  className="stars absolute z-0 size-full bg-[url('/stars.png')] bg-cover bg-bottom"></motion.div>
+      <motion.div className="mountains absolute z-10 size-full bg-[url('/mountains.png')] bg-contain bg-bottom bg-no-repeat md:bg-cover"></motion.div>
+      <motion.div
+        style={{ y: yBg }}
+        className={`planets absolute z-[5] size-full ${type === 'services' ? "bg-[url('/planets.png')]" : "bg-[url('/sun.png')]"} bg-contain bg-bottom bg-no-repeat md:bg-cover`}
+      ></motion.div>
+      <motion.div
+        style={{ x: yBg }}
+        className="stars absolute z-0 size-full bg-[url('/stars.png')] bg-cover bg-fixed bg-bottom"
+      ></motion.div>
     </div>
   );
 };
