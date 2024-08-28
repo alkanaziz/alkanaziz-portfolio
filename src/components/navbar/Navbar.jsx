c8a5d521008d881abe9bc36a629151defdb01f08
alkanaziz <alkanaziz@gmail.com>
2024-08-28 18:30:42 +0200

    refactor: Update Navbar component to include social media links and navigation links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,35 +1,40 @@
 import { FaGithub } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa6';
-import { FaMedium } from 'react-icons/fa6';
 import { motion } from 'framer-motion';
 import Sidebar from '../sidebar/Sidebar';
 
 const Navbar = () => {
+  const links = ['Homepage', 'Services', 'Portfolio', 'About', 'Contact'];
+
   return (
     <div className="navbar h-24">
-      {/* Sidebar */}
-      <Sidebar />
-      <div className="wrapper m-auto flex h-full max-w-screen-xl items-center md:justify-between justify-start p-5">
-        <motion.span
+      <div className="wrapper m-auto flex h-full max-w-screen-xl items-center justify-start p-5 md:justify-between">
+        <div className="social flex gap-5">
+          <a href="https://github.com/alkanaziz" target="_blank">
+            <FaGithub className="hover:scale-110 active:scale-95" size={24} />
+          </a>
+          <a href="https://www.linkedin.com/in/alkanaziz/" target="_blank">
+            <FaLinkedin className="hover:scale-110 active:scale-95" size={24} />
+          </a>
+        </div>
+        <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="font-bold hidden md:block"
+          className="hidden font-bold md:flex md:gap-5"
         >
-          Web Developer Alkan
-        </motion.span>
-        <div className="social flex gap-5">
-          <a href="#">
-            <FaGithub size={20} />
-          </a>
-          <a href="#">
-            <FaLinkedin size={20} />
-          </a>
-          <a href="#">
-            <FaMedium size={20} />
-          </a>
-        </div>
+          {links.map((link) => (
+            <a
+              className="rounded-lg px-2 py-1 text-lg font-semibold duration-200 ease-in hover:bg-slate-300 hover:text-slate-900"
+              href={`#${link.toLowerCase()}`}
+              key={link}
+            >
+              {link}
+            </a>
+          ))}
+        </motion.div>
       </div>
+      <Sidebar />
     </div>
   );
 };
