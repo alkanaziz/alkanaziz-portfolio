73f364f64196e6bb8524d1093312214721cac06a
alkanaziz <alkanaziz@gmail.com>
2024-08-28 18:31:27 +0200

    refactor: Update Hero component layout for better responsiveness

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,4 +1,5 @@
-import { motion, stagger } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { RiScrollToBottomFill } from 'react-icons/ri';
 
 const textVariants = {
   initial: {
@@ -39,13 +40,13 @@ const sliderVariants = {
 
 const Hero = () => {
   return (
-    <div className="hero relative h-[calc(100vh-96px)] overflow-hidden bg-gradient-to-b from-slate-900 to-slate-800">
-      <div className="wrapper m-auto flex h-full max-w-screen-xl flex-col md:flex-row items-center">
+    <div className="hero relative h-[calc(100vh-96px)] overflow-hidden bg-gradient-to-b from-slate-900 to-slate-800 p-5">
+      <div className="wrapper m-auto flex h-full max-w-screen-xl flex-col items-center md:flex-row md:justify-between">
         <motion.div
           variants={textVariants}
           initial="initial"
           animate="animate"
-          className="textContainer flex w-full h-1/2 md:h-full md:w-1/2 flex-col justify-center items-center md:items-start gap-5 md:gap-10"
+          className="textContainer flex h-1/2 w-full flex-col items-center justify-center gap-5 md:h-full md:w-1/2 md:items-start md:gap-10"
         >
           <motion.h2
             variants={textVariants}
@@ -53,43 +54,74 @@ const Hero = () => {
           >
             ALKAN AZIZ
           </motion.h2>
-          <motion.h1 variants={textVariants} className="text-4xl md:text-8xl">
+          <motion.h1 variants={textVariants} className="text-4xl md:text-7xl">
             Web Developer
           </motion.h1>
-          <motion.div variants={textVariants} className="buttons z-10 flex gap-5">
-            <motion.button
-              variants={textVariants}
-              className="rounded-xl border p-5 cursor-pointer"
-            >
-              See the Latest Works
-            </motion.button>
-            <motion.button
-              variants={textVariants}
-              className="rounded-xl border p-5 cursor-pointer"
-            >
-              Contact Me
-            </motion.button>
+          <motion.div variants={textVariants} animate="scrollButton">
+            <RiScrollToBottomFill className="size-10" />
           </motion.div>
-          <motion.img
+        </motion.div>
+        <motion.div variants={textVariants} className="buttons z-10 flex gap-5">
+          <motion.button
             variants={textVariants}
-            animate="scrollButton"
-            className="size-10"
-            src="/scroll.png"
-            alt="scroll icon"
-          />
+            initial="initial"
+            animate="animate"
+            className="cursor-pointer rounded-xl border p-2 text-sm md:p-5 md:text-lg"
+          >
+            See the Latest Works
+          </motion.button>
+          <motion.button
+            variants={textVariants}
+            initial="initial"
+            animate="animate"
+            className="cursor-pointer rounded-xl border p-2 text-sm md:p-5 md:text-lg"
+          >
+            Contact Me
+          </motion.button>
         </motion.div>
       </div>
       <motion.div
         variants={sliderVariants}
         initial="initial"
         animate="animate"
-        className="slidingTextContainer w-1/2 font-bold absolute -bottom-32 flex gap-4 whitespace-nowrap text-[50vh] text-slate-300/5"
+        className="slidingTextContainer absolute -bottom-32 flex w-1/2 gap-4 whitespace-nowrap text-[50vh] font-bold text-slate-300/5"
       >
-        <div className="slidingText">alkan aziz</div>
+        <div className="slidingTex">
+          <span className="transform duration-1000 ease-in hover:text-slate-700">
+            a
+          </span>
+          <span className="transform duration-1000 ease-in hover:text-slate-700">
+            l
+          </span>
+          <span className="transform duration-1000 ease-in hover:text-slate-700">
+            k
+          </span>
+          <span className="transform duration-1000 ease-in hover:text-slate-700">
+            a
+          </span>
+          <span className="transform duration-1000 ease-in hover:text-slate-700">
+            n
+          </span>
+          <span className="transform duration-1000 ease-in hover:text-slate-700">
+            {' '}
+          </span>
+          <span className="transform duration-1000 ease-in hover:text-slate-700">
+            a
+          </span>
+          <span className="transform duration-1000 ease-in hover:text-slate-700">
+            z
+          </span>
+          <span className="transform duration-1000 ease-in hover:text-slate-700">
+            i
+          </span>
+          <span className="transform duration-1000 ease-in hover:text-slate-700">
+            z
+          </span>
+        </div>
       </motion.div>
-      <div className="absolute md:right-0 md:top-0 bottom-0 h-1/2 w-full md:w-max md:h-full">
+      {/* <div className="absolute md:right-0 md:top-0 bottom-0 h-1/2 w-full md:w-max md:h-full">
         <img className='object-cover' src="/hero.png" alt="hero man picture" />
-      </div>
+      </div> */}
     </div>
   );
 };
