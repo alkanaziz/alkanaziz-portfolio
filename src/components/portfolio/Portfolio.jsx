a60130e378a72116e852f4a0b85c7fc4ed575358
alkanaziz <alkanaziz@gmail.com>
2024-09-04 12:17:28 +0200

    refactor: Update Portfolio component to include new projects and links

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,30 +1,47 @@
 import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
 import { useRef } from 'react';
+import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa';
 
 const projects = [
   {
     id: 1,
-    title: 'React Commerce',
-    img: 'https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+    title: 'Speaker Shop Layout',
+    img: '/img/Project-1.png',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+    href: 'https://alkanaziz.github.io/layout-speaker-shop/',
+    github: 'https://github.com/alkanaziz/layout-speaker-shop',
   },
   {
     id: 2,
-    title: 'Next.js Blog',
-    img: 'https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+    title: 'Unit Converter',
+    img: '/img/Project-2.png',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+    href: 'https://alkanaziz.github.io/unit-converter-javascript/',
+    github: 'https://github.com/alkanaziz/unit-converter-javascript/',
   },
   {
     id: 3,
-    title: 'Vanilla JS App',
-    img: 'https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+    title: 'Freelance Developer Page Layout',
+    img: '/img/Project-3.png',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+    href: 'https://alkanaziz.github.io/freelance-developer-page-layout/',
+    github: 'https://github.com/alkanaziz/freelance-developer-page-layout',
   },
   {
     id: 4,
-    title: 'Music App',
-    img: 'https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    title: 'Transparent Login Page',
+    img: '/img/Project-4.png',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+    href: 'https://alkanaziz.github.io/data-transparent-login/',
+    github: 'https://github.com/alkanaziz/data-transparent-login',
+  },
+  {
+    id: 5,
+    title: 'UI - UX Demo Page',
+    img: '/img/Project-5.png',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+    href: 'https://alkanaziz.github.io/ui-ux-nonamepeople-page/',
+    github: 'https://github.com/alkanaziz/ui-ux-nonamepeople-page',
   },
 ];
 
@@ -70,19 +87,51 @@ const SingleProject = ({ project }) => {
   return (
     <section className="project h-screen snap-center">
       <div className="container m-auto flex size-full items-center justify-center overflow-hidden">
-        <div className="wrapper mx-5 mt-36 flex h-full max-w-7xl flex-col items-center justify-center md:gap-12 md:m-0 md:flex-row">
+        <div className="wrapper mx-5 mt-36 flex h-full max-w-7xl flex-col items-center justify-center md:m-0 md:flex-row md:gap-12">
           <motion.div
             variants={imgVariants}
             initial="initial"
             whileInView="animate"
-            className="imageContainer h-1/2 max-h-80 w-full flex-1"
+            className="imageContainer group flex h-1/2 max-h-96 max-w-96 flex-1 items-center justify-center overflow-hidden rounded-xl hover:border"
             ref={ref}
           >
             <img
               src={project.img}
               alt={project.title}
-              className="size-full object-contain md:object-cover"
+              className="w-11/12 rounded-xl shadow-2xl transition-all duration-500 ease-in-out group-hover:rotate-12 group-hover:scale-125 group-hover:brightness-50"
             />
+            <div className="absolute hidden h-80 w-72 sm:w-80 flex-col justify-evenly transition-all ease-in-out group-hover:flex">
+              <h3 className="text-2xl font-black text-orange-400">
+                <a
+                  href={project.href}
+                  target="_blank"
+                  aria-label={`See ${project.title} demo`}
+                  className='[text-shadow:1px_1px_orange]'
+                >
+                  {project.title}
+                </a>
+              </h3>
+              <div className="flex gap-2 text-slate-200 text-sm md:text-lg">
+                <a
+                  href={project.href}
+                  target="_blank"
+                  aria-label={`See ${project.title} demo`}
+                >
+                  <button className="flex gap-2 justify-center items-center rounded-lg bg-orange-400 p-2 hover:bg-orange-300 hover:text-slate-800">
+                    See Demo <FaExternalLinkAlt className="size-4" />
+                  </button>
+                </a>
+                <a
+                  href={project.github}
+                  target="_blank"
+                  aria-label={`See ${project.title} code`}
+                >
+                  <button className="flex gap-2 justify-center items-center rounded-lg bg-orange-400 p-2 hover:bg-orange-300 hover:text-slate-800">
+                    See Code <FaGithub className="size-4" />
+                  </button>
+                </a>
+              </div>
+            </div>
           </motion.div>
           <motion.div
             className="textContainer flex flex-1 flex-col items-center gap-8 md:items-start md:justify-center"
