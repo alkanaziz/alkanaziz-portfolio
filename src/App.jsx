a6b8475cb417943ea12e0ab7793ae11aba333685
alkanaziz <alkanaziz@gmail.com>
2024-09-09 14:07:15 +0200

    refactor: Add Footer component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './app.scss';
 import Contact from './components/contact/Contact';
 import Cursor from './components/cursor/Cursor';
+import Footer from './components/footer/Footer';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
@@ -11,22 +12,23 @@ export default function App() {
   return (
     <div>
       <Cursor />
-      <section id="home" className="lg:h-screen snap-center">
+      <section id="home" className="h-screen snap-center">
         <Navbar />
         <Hero />
       </section>
       <section id="skills" className="h-screen snap-center">
         <Parallax type="skills" />
       </section>
-      <section className="lg:h-screen snap-center">
+      <section className="h-screen snap-center">
         <Skills />
       </section>
       <section id="portfolio" className="h-screen snap-center">
         <Parallax type="portfolio" />
       </section>
       <Portfolio />
-      <section id="contact" className="lg:h-screen snap-center">
+      <section id="contact" className="snap-center h-screen">
         <Contact />
+        <Footer />
       </section>
     </div>
   );
