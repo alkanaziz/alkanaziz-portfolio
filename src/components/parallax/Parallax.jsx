af60f5b4b3b84952dfd80e8acad33b528025771c
alkanaziz <alkanaziz@gmail.com>
2024-09-13 12:21:19 +0200

    refactor: Add scroll animation to Parallax component

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import { RiScrollToBottomFill } from 'react-icons/ri';
 
 const Parallax = ({ type }) => {
   const ref = useRef();
@@ -11,17 +12,33 @@ const Parallax = ({ type }) => {
   const yText = useTransform(scrollYProgress, [0, 1], ['0%', '500%']);
   const yBg = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
 
+  const scrollAnimation = {
+    scrollButton: {
+      opacity: 0,
+      y: 10,
+      transition: {
+        duration: 2,
+        repeat: Infinity,
+      },
+    },
+  };
+
   return (
     <div
       ref={ref}
       className={`parallax relative flex size-full items-center justify-center overflow-hidden bg-gradient-to-b from-slate-800 ${type === 'skills' ? 'to-slate-900' : 'to-slate-700'}`}
     >
-      <motion.h1
-        style={{ y: yText }}
-        className="text-center text-6xl md:text-8xl"
-      >
-        {type === 'skills' ? 'What I Can?' : 'What I Did?'}
-      </motion.h1>
+      <motion.div className="flex flex-col items-center justify-center">
+        <motion.h1
+          style={{ y: yText }}
+          className="text-center text-6xl md:text-8xl"
+        >
+          {type === 'skills' ? 'What I Can?' : 'What I Did?'}
+        </motion.h1>
+        <motion.div variants={scrollAnimation} animate="scrollButton">
+          <RiScrollToBottomFill className="size-10" />
+        </motion.div>
+      </motion.div>
       <motion.div className="mountains absolute z-10 size-full bg-[url('/mountains.png')] bg-contain bg-bottom bg-no-repeat md:bg-cover"></motion.div>
       <motion.div
         style={{ y: yBg }}
